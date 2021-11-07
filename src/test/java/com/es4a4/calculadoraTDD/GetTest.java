74e067fdc7566dc3dcc6b9c30b1af53446523830
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2021-11-07 19:37:31 -0300

    refatorando testes

--- src/test/java/com/es4a4/calculadoraTDD/GetTest.java
+++ src/test/java/com/es4a4/calculadoraTDD/GetTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.es4a4.calculadoraTDD.io.dweet.PostDweet;
 import org.junit.Test;
 
 import com.es4a4.calculadoraTDD.io.dweet.GetDweet;
@@ -11,39 +12,47 @@ import com.es4a4.calculadoraTDD.io.dweet.GetDweet;
 public class GetTest {
 	
 	@Test
-	public void testarGetSomaDweet() {
+	public void testarGetSomaDweet() throws InterruptedException {
+		Thread.sleep(1000);
 		List<String> list = new ArrayList<>();
 		list.add("somar");
 		list.add("10");
 		list.add("5");
+		PostDweet.post("somar", 10, 5);
 		assertEquals(list, GetDweet.get("es4a4-fgmtem"));
 	}
 
 	
 	@Test
-	public void testarGetSubtracaoDweet() {
+	public void testarGetSubtracaoDweet() throws InterruptedException {
+		Thread.sleep(1000);
 		List<String> list = new ArrayList<>();
 		list.add("subtrair");
 		list.add("10");
 		list.add("5");
+		PostDweet.post("subtrair", 10, 5);
 		assertEquals(list, GetDweet.get("es4a4-fgmtem"));
 	}
 	
 	@Test
-	public void testarGetMultiplicarDweet() {
+	public void testarGetMultiplicarDweet() throws InterruptedException {
+		Thread.sleep(1000);
 		List<String> list = new ArrayList<>();
 		list.add("multiplicar");
 		list.add("10");
 		list.add("5");
+		PostDweet.post("multiplicar", 10, 5);
 		assertEquals(list, GetDweet.get("es4a4-fgmtem"));
 	}
 	
 	@Test
-	public void testarGetDividirDweet() {
+	public void testarGetDividirDweet() throws InterruptedException {
+		Thread.sleep(1000);
 		List<String> list = new ArrayList<>();
 		list.add("dividir");
 		list.add("10");
 		list.add("5");
+		PostDweet.post("dividir", 10, 5);
 		assertEquals(list, GetDweet.get("es4a4-fgmtem"));
 	}
 }
